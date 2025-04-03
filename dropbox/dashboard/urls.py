93a089eac2d111afbc200dcb66d83387dc3bd802
xxruyle <xrcnerd@gmail.com>
2025-04-02 22:19:06 -0500

    Add initial base template and dashboard

--- dropbox/dashboard/urls.py
+++ dropbox/dashboard/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='dashboard'),
+    path('', views.home, name='home'),
     path('dashboard/<int:dropbox_id>', views.dashboard, name='dashboard'),
     path('export/<int:dropbox_id>', views.export, name='export'),
     path('api/scandata', views.receive_sensor_data, name='receive_sensor_data'),
